2d116da7cbd20c2455e4f69d6c1c06ddd26f734a
Ahmed Sherif <ahmed.sherif.a.2008@gmail.com>
2023-01-19 14:02:24 +0200

    Solved Problem 281A Word Capitalization

---
+++ Codeforces/281A Word Capitalization.cpp
@@ -0,0 +1,16 @@
+//Solution by: Ahmed Sherif
+//Date/Time: 30/08/2022 - 18:30
+//Verdict: Accepted
+//Problem Link: https://codeforces.com/problemset/problem/281/A
+//Profile Link: https://codeforces.com/profile/AhmeddSherif
+
+#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+    string s; cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s << endl;
+}
