bb545e0311c41ca6b54b74958224a7e9849dab69
Ahmed Sherif <ahmed.sherif.a.2008@gmail.com>
2023-01-23 20:53:23 +0200

    Solved HackerRank Problem Frequency Queries

---
+++ HackerRank/Frequency Queries.cpp
@@ -0,0 +1,42 @@
+//Solution by: Ahmed Sherif
+//Date/Time: 23/01/2023 20:50
+//Verdict: Accepted
+//Problem Link: https://www.hackerrank.com/challenges/frequency-queries/problem
+//Profile Link: https://www.hackerrank.com/Ahmed_Sherif
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    unordered_map<int, int> freq;
+    unordered_map<int, int> nums;
+    int op, x;
+    for (int i = 0; i < n; i++) {
+        cin >> op >> x;
+        if (op == 1) {
+            if (nums.count(x)) {
+                freq[nums[x]]--;
+                nums[x]++;
+                freq[nums[x]]++;
+            } else {
+                nums[x] = 1;
+                freq[nums[x]]++;
+            }
+        } else if (op == 2) {
+            if (nums.count(x) && nums[x] > 0) {
+                freq[nums[x]]--;
+                nums[x]--;
+                if (nums[x] > 0) freq[nums[x]]++;
+            }
+        } else if (op == 3) {
+            if (freq.count(x) && freq[x] > 0) cout << 1 << endl;
+            else cout << 0 << endl;
+        }
+    }
+    return 0;
+}
