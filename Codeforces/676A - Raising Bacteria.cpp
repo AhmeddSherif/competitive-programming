e077234c1ec22867900ca6abba477a4aafd93ab6
Ahmed Sherif <ahmed.sherif.a.2008@gmail.com>
2023-02-13 21:58:42 +0200

    Solved Problem 676A - Raising Bacteria

---
+++ Codeforces/676A - Raising Bacteria.cpp
@@ -0,0 +1,12 @@
+//Solution by: Ahmed Sherif
+//Date/Time: 13/02/2023 - 21:34
+//Verdict: Accepted
+//Problem Link: https://codeforces.com/problemset/problem/579/A
+//Profile Link: https://codeforces.com/profile/AhmeddSherif
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a; cin >> a;
+    cout << __builtin_popcount(a);
+}
