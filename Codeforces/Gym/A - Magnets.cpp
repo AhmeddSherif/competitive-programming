feb226b971a6c23ad698c4d20946adc140024ebb
Ahmed Sherif <ahmed.sherif.a.2008@gmail.com>
2023-06-21 10:23:11 +0300

    Solved Problem A- Magnets (Gym)

--- Codeforces/Gym/A - Magnets.cpp
+++ Codeforces/Gym/A - Magnets.cpp
@@ -1,25 +1,29 @@
 // Solution by: Ahmed Sherif
-// Date/Time: 21/06/2023 - 09:42
+// Date/Time: 21/06/2023 - 10:17
 // Verdict: Accepted
-// Problem Link: https://codeforces.com/contest/405/problem/A
+// Problem Link: https://codeforces.com/contest/344/problem/A
 // Profile Link: https://codeforces.com/profile/AhmeddSherif
 
 #include<iostream>
 #include<vector>
-#include<algorithm>
-
+ 
 #define MADMAX ios_base::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 typedef long long ll;
-
+ 
 int main() {
     MADMAX;
 	int n; cin >> n;
-	vector<int> v(n);
-	for (int i = 0; i < n; i++) cin >> v[i];
-	sort(v.begin(), v.end());
+	int ans = 0;
+	vector<int> v;
+
 	for (int i = 0; i < n; i++)
 	{
-		cout << v[i] << " ";
+		int x; cin >> x;
+		v.push_back(x);
 	}
+	for (int i = 0; i < n - 1; i++)
+		if (v[i] != v[i + 1]) ans++;
+
+	cout << ans + 1;
 }
